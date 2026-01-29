76ec5aa6469b058be79ff4216c1eb0c6df194c76
saitehith28 <saitehith028@gmail.com>
2026-01-29 21:10:19 +0530

    commiting

--- Shell Scripting/06-dynamic.sh
+++ Shell Scripting/06-dynamic.sh
@@ -4,6 +4,6 @@ START_TIME=$(date +%s)
 echo "Script started at: $START_TIME"
 
 sleep 10
-END_TIME=$(dat +%s)
+END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "Script executed in $TOTAL_TIME"
