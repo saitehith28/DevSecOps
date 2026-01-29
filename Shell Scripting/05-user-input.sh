ceb5b00a19b08d573190340d990f9b8169614c04
saitehith28 <saitehith028@gmail.com>
2026-01-29 21:03:14 +0530

    commiting

---
+++ Shell Scripting/05-user-input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your Name:"
+read USER_NAME
+echo "User Name is $USER_NAME"
+
+echo "Please enter the Password:"
+read -s PASSWORD
+echo "User Password is $PASSWORD"
