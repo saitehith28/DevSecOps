8af62bb24ec4508cb08263d4ac17b8d9d4279821
saitehith28 <saitehith028@gmail.com>
2026-01-29 22:08:42 +0530

    commiting

---
+++ Shell Scripting/10-installations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "Please run with root user access"
+    exit 1
+fi
+echo "Installing"
+dnf install nginx -y
+
+if [ $? -ne 0]; then
+    echo "FAILURE"
+    exit 1
+else
+    echo "SUCCESS"
+fi
