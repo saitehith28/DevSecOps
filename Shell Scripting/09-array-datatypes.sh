eb7b4697cc9893b5c514902ce5548a704880f722
saitehith28 <saitehith028@gmail.com>
2026-01-29 21:55:31 +0530

    commiting

---
+++ Shell Scripting/09-array-datatypes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FRUITS=("Apple","Banana","Orange")
+echo "Fruits are ${FRUITS[@]}"
+echo "First Fruit is ${FRUITS[0]}"
+echo "Second Fruit is ${FRUITS[1]}"
+echo "Third Fruit is ${FRUITS[2]}"
