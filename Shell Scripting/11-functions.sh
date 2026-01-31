9330a904f43a4f0e101a39d6d7648a1d8380f013
saitehith28 <saitehith028@gmail.com>
2026-01-31 17:57:04 +0530

    commiting

---
+++ Shell Scripting/11-functions.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Please try with root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 FAILURE"
+        exit 1
+    else
+        echo "$2 SUCCESS"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing MySql"
