626c2b38d1ff3708d2fce9feab78260a220098f8
saitehith28 <saitehith028@gmail.com>
2026-02-01 15:50:28 +0530

    commiting

--- Shell Scripting/17-colors.sh
+++ Shell Scripting/17-colors.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shell-scripting"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "$R Please try with root user$N" | tee -a $LOGS_FILE
+    echo -e "$R Please try with root user$N" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -29,10 +29,10 @@ for package in $@
 do
     dnf list installed $package &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
-        echo "$B $package not installed, Installing...$N" | tee -a $LOGS_FILE
+        echo -e "$B $package not installed, Installing...$N" | tee -a $LOGS_FILE
         dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "Installing $package"
     else
-        echo "$G $package already installed, skipping $N" | tee -a $LOGS_FILE
+        echo -e "$G $package already installed, skipping $N" | tee -a $LOGS_FILE
     fi
 done
