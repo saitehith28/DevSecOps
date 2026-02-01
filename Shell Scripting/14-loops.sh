c2aebb735ff88d027825967a84716db488ccd271
saitehith28 <saitehith028@gmail.com>
2026-02-01 15:14:04 +0530

    commiting

---
+++ Shell Scripting/14-loops.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripting"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please try with root user" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf install $package -y &>>LOGS_FILE
+    VALIDATE $? "Installing $package"
+done
