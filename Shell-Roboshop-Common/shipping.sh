9675446d06f537c3178f1070fd198853432b29bf
saitehith28 <saitehith028@gmail.com>
2026-02-03 23:57:48 +0530

    commiting

--- Shell-Roboshop-Common/shipping.sh
+++ Shell-Roboshop-Common/shipping.sh
@@ -8,6 +8,9 @@ app_setup
 java_setup
 systemd_setup
 
+dnf install mysql -y  &>>$LOGS_FILE
+VALIDATE $? "Installing MySQL"
+
 # mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
 # mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
 # mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
