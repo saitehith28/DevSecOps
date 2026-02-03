e3404803ea6b19fab78ae057fdb99812888738a9
saitehith28 <saitehith028@gmail.com>
2026-02-03 09:45:16 +0530

    commiting

--- Shell-Roboshop/shipping.sh
+++ Shell-Roboshop/shipping.sh
@@ -70,14 +70,17 @@ VALIDATE $? "Reload Enable and Starting shipping"
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing Mysql"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
-if [ $? -ne 0 ]; then
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
-else
-    echo -e "Data is already loaded ... $Y SKIPPING $N"
-fi
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+# mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+# if [ $? -ne 0 ]; then
+#     mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+#     mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+#     mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+# else
+#     echo -e "Data is already loaded ... $Y SKIPPING $N"
+# fi
 
 systemctl restart shipping &>>$LOGS_FILE
 VALIDATE $? "Restarting Shipping"
