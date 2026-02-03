a05cd1fb2e6d36327065ca21badde9187736cd38
saitehith28 <saitehith028@gmail.com>
2026-02-03 09:14:16 +0530

    commiting

--- Shell-Roboshop/redis.sh
+++ Shell-Roboshop/redis.sh
@@ -31,7 +31,7 @@ dnf module enable redis:7 -y &>>$LOGS_FILE
 VALIDATE $? "Enabling Redis:7"
 
 #s/protected-mode yes/protected-mode no
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf -y &>>$LOGS_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOGS_FILE
 VALIDATE $? "Allowing Remote Connections"
 
 systemctl enable redis &>>$LOGS_FILE
