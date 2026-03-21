8e57b70d5910f367063d78d7addcf7c237a5b15d
saitehith28 <saitehith028@gmail.com>
2026-03-21 22:01:17 +0530

    Test Terraform

--- Terraform/terraform-aws-vpc-test/vpc.tf
+++ Terraform/terraform-aws-vpc-test/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc"{
-    source="git::https://github.com/saitehith28/Terraform-Modules.git//terraform-aws-vpc?href=main"
+    source="git::https://github.com/saitehith28/Terraform-Modules.git//terraform-aws-vpc?ref=main"
     project=var.project
     environment=var.environment
     is_peering_required=true
