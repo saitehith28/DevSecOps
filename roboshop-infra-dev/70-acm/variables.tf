4eb408e03f1cebdec0d51b69bbaa7793715cde95
saitehith28 <saitehith028@gmail.com>
2026-03-29 23:32:30 +0530

    terraform

---
+++ roboshop-infra-dev/70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07424761Y3CDV3OLV2IR"
+}
+
+variable "domain_name" {
+    default = "daws88s.online"
+}
